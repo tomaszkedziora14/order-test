017adf9658b76952a8cba091dd9716f6727a30c8
tomaszkedziora14 <tomaszkedziora14@wp.pl>
2023-05-17 22:57:32 +0200

    Update OrderProcessingTest.php

--- tests/Functional/OrderProcessingTest.php
+++ tests/Functional/OrderProcessingTest.php
@@ -11,13 +11,19 @@ class OrderProcessingTest extends WebTestCase
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/orders');
+        
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        
         $form = $crawler->selectButton('Process')->form();
         $form['channel1_orders'] = '10';
         $form['channel2_orders'] = '5';
+        
         $client->submit($form);
+        
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        
         $crawler = $client->followRedirect();
+        
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Orders processed successfully', $crawler->filter('h1')->text());
     }
